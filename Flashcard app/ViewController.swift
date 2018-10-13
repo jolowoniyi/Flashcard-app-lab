6d10d2ed02089173f7971b3e649bd7699601702e
Joy olowoniyi <joyolowoniyi@joys-macbook-pro.local>
2018-10-13 13:51:02 -0700

    Initial Commit

---
+++ Flashcard app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flashcard app
+//
+//  Created by Joy olowoniyi on 10/13/18.
+//  Copyright © 2018 Joy olowoniyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
